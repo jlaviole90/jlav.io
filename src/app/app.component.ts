c0760eec742f9290676ba7ca7237a24bd38c6956
jlaviole90 <jlaviole90@gmail.com>
2025-03-16 19:30:08 -0500

    add vercel analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { inject } from "@vercel/analytics";
 
 @Component({
     selector: 'app-root',
@@ -9,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
     title = 'jlav.io';
+
+    constructor() {
+        inject();
+    }
 }
