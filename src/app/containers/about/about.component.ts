380bce5aab335f52de23b28dfb2c4e9ef7c1b8dd
jlaviole90 <jlaviole90@gmail.com>
2026-03-14 20:01:00 -0500

    Update about me page

--- src/app/containers/about/about.component.ts
+++ src/app/containers/about/about.component.ts
@@ -1,39 +1,15 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-type FlipState = 'front' | 'flipping-to-back' | 'back' | 'flipping-to-front';
-
 @Component({
     selector: 'app-about',
     templateUrl: './about.component.html',
     styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent {
-    flipState: FlipState = 'front';
-
     constructor(private readonly router: Router) {}
-
-    get isBack(): boolean {
-        return this.flipState === 'back' || this.flipState === 'flipping-to-front';
-    }
 
     navigate(path: string): void {
         this.router.navigate([path]);
-    }
-
-    flip(): void {
-        if (this.flipState === 'front') {
-            this.flipState = 'flipping-to-back';
-        } else if (this.flipState === 'back') {
-            this.flipState = 'flipping-to-front';
-        }
-    }
-
-    onFlipEnd(): void {
-        if (this.flipState === 'flipping-to-back') {
-            this.flipState = 'back';
-        } else if (this.flipState === 'flipping-to-front') {
-            this.flipState = 'front';
-        }
     }
 }
